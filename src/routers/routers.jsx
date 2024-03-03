a1a7725706592099c6bd661a32ebb90b9e8a9b1b
rirefat <rirefat.official@gmail.com>
2024-03-03 13:24:51 +0600

    done registration page

--- src/routers/routers.jsx
+++ src/routers/routers.jsx
@@ -2,14 +2,16 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Login from "../components/Login/Login";
 import ErrorPage from "../components/ErrorPage/ErrorPage";
+import Registration from "../components/Registration/Registration";
 
 const routers = createBrowserRouter([
     {
-        path: '/', 
-        element:<App/>,
-        errorElement: <ErrorPage/>,
+        path: '/',
+        element: <App />,
+        errorElement: <ErrorPage />,
         children: [
-            {path: 'login' , element:<Login/>}
+            { path: 'login', element: <Login /> },
+            { path: 'registration', element: <Registration /> },
         ]
     }
 ])
