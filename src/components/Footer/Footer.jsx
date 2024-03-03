eadc35085c83189c803d7e660155401f94b710e0
rirefat <rirefat.official@gmail.com>
2024-03-03 13:20:35 +0600

    done login page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
     return (
-        <div>
-            <h1>Footer</h1>
-        </div>
+        <footer className="text-center py-8 text-sm text-slate-600">
+            <p>Copyright || All Rights Reserved</p>
+        </footer>
     );
 };
 
